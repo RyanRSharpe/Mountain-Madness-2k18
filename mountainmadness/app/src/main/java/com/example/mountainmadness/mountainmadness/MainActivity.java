affd1e2df7d39131b348f2d03de634f120a96aaf
RyanRSharpe <35910833+RyanRSharpe@users.noreply.github.com>
2018-02-17 14:47:31 -0800

    map

--- mountainmadness/app/src/main/java/com/example/mountainmadness/mountainmadness/MainActivity.java
+++ mountainmadness/app/src/main/java/com/example/mountainmadness/mountainmadness/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
 //                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
 //                        .setAction("Action", null).show();
-                setContentView(R.layout.activity_maps);  
+                setContentView(R.layout.activity_maps);
             }
         });
     }
