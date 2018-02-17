9413949c76865df8faa218f1250fb5242bc3279d
RyanRSharpe <35910833+RyanRSharpe@users.noreply.github.com>
2018-02-17 14:32:30 -0800

    press dat button

--- mountainmadness/app/src/main/java/com/example/mountainmadness/mountainmadness/MainActivity.java
+++ mountainmadness/app/src/main/java/com/example/mountainmadness/mountainmadness/MainActivity.java
@@ -22,8 +22,9 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+                setContentView(R.layout.activity_maps);  
             }
         });
     }
