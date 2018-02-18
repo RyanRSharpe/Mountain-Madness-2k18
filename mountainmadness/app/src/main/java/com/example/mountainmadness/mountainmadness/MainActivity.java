d210612d2981ffca2f8dc65448daf2b2fd3a8a18
RyanRSharpe <35910833+RyanRSharpe@users.noreply.github.com>
2018-02-17 17:30:43 -0800

    more

--- mountainmadness/app/src/main/java/com/example/mountainmadness/mountainmadness/MainActivity.java
+++ mountainmadness/app/src/main/java/com/example/mountainmadness/mountainmadness/MainActivity.java
@@ -24,9 +24,11 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
 //                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
 //                        .setAction("Action", null).show();
-                setContentView(R.layout.activity_maps);
+                Intent startIntent = new Intent(getApplicationContext(), MapsActivity.class);
+                startActivity(startIntent);
             }
         });
+
         Button button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
--- mountainmadness/app/src/main/java/com/example/mountainmadness/mountainmadness/MapsActivity.java
+++ mountainmadness/app/src/main/java/com/example/mountainmadness/mountainmadness/MapsActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
