845fe9882cc0d231d2588a9c2d48ee84c24f1754
plasmalaser <fubof@sfu.ca>
2018-02-17 22:38:30 -0800

    dynamic list scrollable and done ooooooo bbbbbbby

--- mountainmadness/app/src/main/java/com/example/mountainmadness/mountainmadness/historyActivity.java
+++ mountainmadness/app/src/main/java/com/example/mountainmadness/mountainmadness/historyActivity.java
@@ -6,6 +6,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import java.util.*;
+import android.widget.*;
 
 public class historyActivity extends AppCompatActivity {
 
@@ -15,6 +17,20 @@ public class historyActivity extends AppCompatActivity {
         setContentView(R.layout.activity_history);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        ArrayList strArr = new ArrayList<String>(0);
+        //Arrays.asList("a", "b", "c", "d", "e")
+        for(int i=0;i<200;i++){
+            strArr.add(Integer.toString(i));
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                R.layout.stock_textview, strArr);
+
+        ListView listView = (ListView) findViewById(R.id.historyList);
+        listView.setNestedScrollingEnabled(true);
+        listView.setAdapter(adapter);
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
