b33502369f9235dee15f4a39220ce92273621a8d
Rusty <rww6@sfu.ca>
2018-02-17 19:57:26 -0800

    changes to maps

--- mountainmadness/app/src/main/java/com/example/mountainmadness/mountainmadness/MapsActivity.java
+++ mountainmadness/app/src/main/java/com/example/mountainmadness/mountainmadness/MapsActivity.java
@@ -1,10 +1,9 @@
 package com.example.mountainmadness.mountainmadness;
 
 import android.content.Intent;
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.View;
-import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -12,8 +11,6 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-
-import static android.support.v4.content.ContextCompat.startActivity;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -44,9 +41,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng SFU = new LatLng(49, -122);
+        mMap.addMarker(new MarkerOptions().position(SFU).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(SFU));
     }
 
         public void sex(View view){
